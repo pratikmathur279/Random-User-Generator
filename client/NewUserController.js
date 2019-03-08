2ff6def099f465f0de25ebbff8dd14169587c7be
Pratik Mathur <pratikmathur279@gmail.com>
2019-03-08 14:45:41 -0500

    changes

--- client/NewUserController.js
+++ client/NewUserController.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 (function () {
     'use strict';
 
@@ -37,43 +37,3 @@
 
 
 })();
-=======
-(function () {
-    'use strict';
-
-    angular
-        .module('app')
-        .controller('NewUserController', NewUserController);
-
-
-    NewUserController.$inject = ['$http'];
-
-    function NewUserController($http) {
-        var newUserVm = this;
-
-        init();
-
-        newUserVm.getUser = getUser;
-
-        function init() {
-            //console.log("NewUserController");
-
-            getUser();
-        }
-
-        function getUser() {
-            $http.get('https://j6u7loc31m.execute-api.us-east-1.amazonaws.com/dev/getuser')
-                .then(function (res) {
-                        newUserVm.user = res.data;
-                        //console.log(newUserVm.user);
-
-                    },
-                    function (error) {
-                        console.log(error);
-                    });
-        }
-    }
-
-
-})();
->>>>>>> ed6c8c1b904722b2f3b8a69025816304f4bc429c
--- client/UserListController.js
+++ client/UserListController.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 (function () {
     'use strict';
 
@@ -49,43 +48,3 @@
 
 
 })();
-=======
-(function () {
-    'use strict';
-
-    angular
-        .module('app')
-        .controller('UserListController', UserListController);
-
-
-    UserListController.$inject = ['$http'];
-
-    function UserListController($http) {
-        var userListVm = this;
-
-        init();
-
-        userListVm.retrieveUsers = retrieveUsers;
-
-        function init() {
-
-            retrieveUsers();
-            //console.log("UserListController");
-
-        }
-
-        function retrieveUsers() {
-            $http.get('https://j6u7loc31m.execute-api.us-east-1.amazonaws.com/dev/retrieveusers')
-                .then(function (res) {
-                        userListVm.users = res.data.Items;
-                        // console.log(userListVm.users);
-                    },
-                    function (error) {
-                        console.log(error);
-                    });
-        }
-    }
-
-
-})();
->>>>>>> ed6c8c1b904722b2f3b8a69025816304f4bc429c
